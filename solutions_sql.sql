35ec1aff675e0ad9110408e2991ecf7d1b33866a
bhavya34-hue <bhatbhavya34@gmail.com>
2026-03-30 15:21:02 +0530

    Refactor Netflix SQL queries and data cleaning

--- solutions_sql.sql
+++ solutions_sql.sql
@@ -69,6 +69,7 @@ SET country = TRIM(country),
 --15 Real based problems
 
 --Q1
+--Count the number of Movies vs TV Shows
 SELECT 
    type,
    COUNT(*) AS total_content
@@ -76,6 +77,7 @@ FROM netflix
 GROUP BY type;
 
 --Q2
+--Find the most common rating for movies and TV shows
 SELECT type, rating
 FROM (
     SELECT 
@@ -89,6 +91,7 @@ FROM (
 WHERE ranking = 1;
 
 --Q3
+--List all movies released in a specific year (e.g., 2020)
 select * 
 from netflix 
 where
@@ -97,6 +100,7 @@ and
 release_year = 2020;
 
 --Q4
+--Find the top 5 countries with the most content on Netflix
 SELECT 
     TRIM(UNNEST(STRING_TO_ARRAY(country, ','))) AS new_country,
     COUNT(show_id) AS total_content
@@ -106,6 +110,7 @@ ORDER BY 2 DESC
 LIMIT 5;
 
 --Q5
+--Identify the longest movie
 SELECT 
     title,
     duration_int
@@ -115,22 +120,26 @@ ORDER BY duration_int DESC
 LIMIT 1;
 
 --Q6
+--ind content added in the last 5 years
 SELECT *
 FROM netflix
 WHERE date_added_clean >= CURRENT_DATE - INTERVAL '5 years';
 
 --Q7
+--Find all the movies/TV shows by director 'Rajiv Chilaka'!
 select *
 from netflix
 where director ilike '%Rajiv Chilaka%';
 
 --Q8
+--List all TV shows with more than 5 seasons
 SELECT *
 FROM netflix
 WHERE type = 'TV Show'
 AND duration_int > 5;
 
 --Q9
+--Count the number of content items in each genre
 select 
 TRIM(UNNEST(STRING_TO_ARRAY(listed_in, ','))),
 count(show_id) as most_content
@@ -138,6 +147,8 @@ from netflix
 group by 1;
 
 --Q10
+--Find each year and the average numbers of content release in India on netflix. 
+return top 5 year with highest avg content release!
 select 
 extract(year from TO_DATE(date_added, 'Month DD, YYYY')) as year, 
 count(*),
@@ -148,22 +159,26 @@ where country ilike '%India%'
 group by 1;
 
 --Q11
+--List all movies that are documentaries
 select * 
 from netflix
 where listed_in like '%Documentaries%';
 
 --Q12 
+--Find all content without a director
 SELECT *
 FROM netflix
 WHERE director IS NULL OR director = '';
 
 --Q13
+--Find how many movies actor 'Salman Khan' appeared in last 10 years!
 select *
 from netflix
 where casts ilike '%Salman Khan%'
 and release_year > extract(year from current_date) -10;
 
 --Q14
+--Find the top 10 actors who have appeared in the highest number of movies produced in India.
 select
 TRIM(UNNEST(STRING_TO_ARRAY(casts, ','))),
 count(*) as total_content
@@ -174,6 +189,9 @@ order by 2 desc
 limit 10;
 
 --Q15
+--Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
+the description field. Label content containing these keywords as 'Bad' and all other 
+content as 'Good'. Count how many items fall into each category.
 SELECT 
     category,
     COUNT(*) AS content_count
